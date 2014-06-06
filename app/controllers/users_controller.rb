19fde56d28605451456209b18dd5a7121e610366
Rich Bowman <BostonREB99@gmail.com>
2014-06-06 12:53:05 -0400

    clean up expired items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
+    @user_auction_items = current_user.items.current
     @user_bidded_on_items = current_user.bids.map(&:item).uniq
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   has_many :bids
+
+  private
+
+  def self.current
+    where("expiration_date > ?", Date.today)
+  end
 end
